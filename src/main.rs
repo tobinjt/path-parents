100e4a94c1fb033c95d53ec4b2f61a7b46ec814b
John Tobin <johntobin@johntobin.ie>
2024-08-04 23:03:16 +0100

    Wrote MultipleFileReader.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,149 @@
+#![allow(dead_code)]
+use std::fs::File;
+use std::io::Read;
+// use std::path::Path;
+// use std::path::PathBuf;
+
+// Outline:
+// - For each line, split it into components, the iterate over those.
+// - Reassemble the components, printing if longer than skip.
+
+/// Read from all the provided files, reading from the next file when the end of the current file
+/// is reached.
+struct MultipleFileReader {
+    filehandles: Vec<Box<dyn Read>>,
+}
+
+impl MultipleFileReader {
+    /// Initialises and returns a MultipleFileReader from a list of filenames.
+    ///
+    /// All the filenames provided will be opened eagerly, so problems related to permissions or
+    /// existence will be detected by new and the error from [File::open](std::fs::File::open)
+    /// returned.
+    fn new(filenames: Vec<String>) -> Result<Self, std::io::Error> {
+        let mut filehandles: Vec<Box<dyn Read>> = Vec::with_capacity(filenames.len());
+        for filename in filenames {
+            filehandles.push(Box::new(File::open(filename)?));
+        }
+        Ok(Self::new_from_filehandles(filehandles))
+    }
+
+    /// Initialises and returns a MultipleFileReader from a list of filehandles (anything
+    /// implementing the [std::io::Read] trait.  Uses the filehandles unchanged, so they can
+    /// point to anything: files, stdin, sockets, ...
+    fn new_from_filehandles(filehandles: Vec<Box<dyn Read>>) -> MultipleFileReader {
+        Self { filehandles }
+    }
+}
+
+/// Implements the [std::io::Read] Trait.
+impl Read for MultipleFileReader {
+    /// - A single read() will not return data from two inputs.
+    /// - Advances to the next input when a read() from the current input file returns 0, so an
+    ///   input file that returns 0 rather than blocking until data is available will not be
+    ///   retried.
+    /// - The current input will be discarded when moving on to the next input, so it will
+    ///   automatically be closed.
+    /// - Errors from underlying read() calls are returned *without* advancing to the next input
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        while !self.filehandles.is_empty() {
+            let length = self.filehandles[0].read(buf)?;
+            if length > 0 {
+                return Ok(length);
+            }
+            // Filehandle has run out of data.
+            self.filehandles.remove(0);
+        }
+        // Run out of files to read.
+        Ok(0)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::BufRead;
+    use std::io::BufReader;
+
+    /// An implementation of  [std::io::Read] that always fails with [std::io::Error] derived
+    /// from std::io::ErrorKind::Other.
+    struct ReadAlwaysFails {}
+
+    impl Read for ReadAlwaysFails {
+        fn read(&mut self, _buf: &mut [u8]) -> std::io::Result<usize> {
+            Err(std::io::Error::new(std::io::ErrorKind::Other, "oh no!"))
+        }
+    }
+
+    #[test]
+    fn test_one_file() {
+        let multi_file_reader =
+            MultipleFileReader::new(vec![String::from("testdata/file1")]).unwrap();
+        let lines: Vec<String> = BufReader::new(multi_file_reader)
+            .lines()
+            .map(|l| l.unwrap())
+            .collect();
+        let expected = vec![
+            String::from("This is file 1."),
+            String::from(""),
+            String::from("It is not very interesting."),
+        ];
+        assert_eq!(expected, lines);
+    }
+
+    #[test]
+    fn test_multiple_files() {
+        let filenames = vec![
+            String::from("testdata/file1"),
+            String::from("testdata/file2"),
+            String::from("testdata/file3"),
+        ];
+        let multi_file_reader = MultipleFileReader::new(filenames).unwrap();
+        let lines: Vec<String> = BufReader::new(multi_file_reader)
+            .lines()
+            .map(|l| l.unwrap())
+            .collect();
+        let expected = vec![
+            String::from("This is file 1."),
+            String::from(""),
+            String::from("It is not very interesting."),
+            String::from("File 2 isn't really any better than file 1."),
+            String::from(""),
+            String::from(""),
+            String::from("It has more blank lines.  Including a trailing blank line."),
+            String::from(""),
+            String::from("File 3 is just here to tell you that the next file is Lorem Ipsum."),
+        ];
+        assert_eq!(expected, lines);
+    }
+
+    #[test]
+    fn test_open_fails() {
+        let filenames = vec![
+            String::from("testdata/file1"),
+            String::from("testdata/file_does_not_exist"),
+            String::from("testdata/file3"),
+        ];
+        let multi_file_reader = MultipleFileReader::new(filenames);
+        assert!(multi_file_reader.is_err());
+    }
+
+    #[test]
+    fn test_read_fails() {
+        // We construct a filehandle that errors followed by a valid filehandle.
+        // Reads should consistently fail rather than moving on to the valid filehandle.
+        let filehandles: Vec<Box<dyn Read>> = vec![
+            Box::new(ReadAlwaysFails {}),
+            Box::new(File::open("testdata/file1").expect("open(testdata/file1) failed?")),
+        ];
+        let mut multi_file_reader = MultipleFileReader::new_from_filehandles(filehandles);
+        let mut buffer = [0; 10];
+        assert!(multi_file_reader.read(&mut buffer).is_err());
+        assert!(multi_file_reader.read(&mut buffer).is_err());
+        assert!(multi_file_reader.read(&mut buffer).is_err());
+    }
 }
