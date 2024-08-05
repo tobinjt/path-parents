c521754c6f040f06d2bcef1eada2a9b984bbf56b
John Tobin <johntobin@johntobin.ie>
2024-08-05 23:20:30 +0100

    Implement parents_of_filename.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 #![allow(dead_code)]
 use std::fs::File;
 use std::io::Read;
-// use std::path::Path;
-// use std::path::PathBuf;
-
-// Outline:
-// - For each line, split it into components, the iterate over those.
-// - Reassemble the components, printing if longer than skip.
+use std::path::Path;
+use std::path::PathBuf;
 
 /// Read from all the provided files, reading from the next file when the end of the current file
 /// is reached.
@@ -59,8 +55,7 @@ impl Read for MultipleFileReader {
     }
 }
 
-/// Returns the lines from a MultipleFileReader (if filenames are provided) or stdin if no
-/// filenames are provided.
+/// Returns a MultipleFileReader (if filenames are provided) or stdin if no filenames are provided.
 struct StdinOrFiles {
     reader: Box<dyn Read>,
 }
@@ -88,6 +83,18 @@ impl Read for StdinOrFiles {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         self.reader.read(buf)
     }
+}
+
+fn parents_of_filename(filename: &str, skip: usize) -> Vec<String> {
+    let mut result: Vec<String> = vec![];
+    let mut path = PathBuf::new();
+    for (i, component) in Path::new(&filename).components().enumerate() {
+        path.push(component);
+        if i > skip {
+            result.push(path.as_path().to_str().unwrap().to_string());
+        }
+    }
+    result
 }
 
 fn main() {
@@ -220,5 +227,26 @@ mod tests_stdin_or_files {
             String::from("testdata/file3"),
         ];
         assert!(StdinOrFiles::new(filenames).is_err());
+    }
+}
+
+#[cfg(test)]
+mod tests_parents_of_filename {
+    use super::*;
+
+    #[test]
+    fn test_all_parents() {
+        let expected = vec![
+            String::from("/usr"),
+            String::from("/usr/bin"),
+            String::from("/usr/bin/cat"),
+        ];
+        assert_eq!(expected, parents_of_filename("/usr/bin/cat", 0));
+    }
+
+    #[test]
+    fn test_skipping() {
+        let expected = vec![String::from("/usr/bin"), String::from("/usr/bin/cat")];
+        assert_eq!(expected, parents_of_filename("/usr/bin/cat", 1));
     }
 }
